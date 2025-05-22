00fcd96c593545fcae4e936221cca9213d4ed55f
wale2009 <adewaleadebayo202@gmail.com>
2025-05-22 03:55:51 +0100

    testing

--- firebase/client.ts
+++ firebase/client.ts
@@ -6,14 +6,15 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtJOveFbI5LWNrf7MdzboPFOx-m_fvRz4",
-  authDomain: "mockmate-8a79f.firebaseapp.com",
-  projectId: "mockmate-8a79f",
-  storageBucket: "mockmate-8a79f.firebasestorage.app",
-  messagingSenderId: "663769559164",
-  appId: "1:663769559164:web:8518624a710a2cd00bd9f9",
-  measurementId: "G-BSQSCCJ44E"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
